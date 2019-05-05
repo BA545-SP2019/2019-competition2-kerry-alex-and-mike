83a228c1dd88fb72671ff08eff3cdf8f0721b9fb
Kerry Clarke <kerry.clarke@student.fairfield.edu>
2019-05-05 00:59:42 +0000

    updated TPOT notebooks

--- pipeline_2/tpot_credit_pipeline2.py
+++ pipeline_2/tpot_credit_pipeline2.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.svm import LinearSVC
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,11 +9,8 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:0.7857683491638691
-exported_pipeline = make_pipeline(
-    VarianceThreshold(threshold=0.005),
-    LogisticRegression(C=20.0, dual=False, penalty="l2")
-)
+# Average CV score on the training set was:0.7876443471560994
+exported_pipeline = LinearSVC(C=0.1, dual=False, loss="squared_hinge", penalty="l2", tol=0.01)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
